58e6e14a0c0c868782c129a8ea6b538e7adcb71c
Chandima Wijerathna <153199095+Chandima0406@users.noreply.github.com>
2025-08-15 21:16:26 +0530

    create product model

---
+++ backend/models/product.model.js
@@ -0,0 +1,25 @@
+import mongoose from "mongoose";
+
+const productchema = mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+    },
+    price: {
+      type: Number,
+      required: true,
+    },
+    image: {
+      type: String,
+      required: true,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+const product = mongoose.model('product', productchema);
+
+export default product;
